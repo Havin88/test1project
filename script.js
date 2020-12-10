02060f4df25f1f6bf7ac04c4dccfcb7260515f62
Dmitry <Havin.dmitry@gmail.com>
2020-12-10 14:35:01 +0300

    second test commit

---
+++ script.js
@@ -0,0 +1,46 @@
+'use strict';
+
+// var leftBorderWidth = 1; 
+// let second = 2;
+// console.log(second);
+// const pi = 3.14;
+
+// console.log(leftBorderWidth);
+
+
+// let persone = {
+//     name: "Dima",
+//     age: 32,
+//     isMarried: false
+// };
+
+// console.log(persone);
+// console.log(persone.age);
+
+// let arr = ['plum.png','orange.jpeg','apple.bmp']
+// .log(arr[0])
+
+// alert("Hello World!")
+
+// let answer = +prompt("Are you 18?", "yes");
+
+// console.log(answer);
+// console.log(typeof(answer));
+
+// console.log("arr" + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+
+//     console.log(incr++);
+//     console.log(decr);
+//     console.log(incr);
+
+console.log(8%3);
+console.log("2" === "2");
+
+let isChecked = true,
+    isClose = true;
+
+console.log(isChecked && isClose);
